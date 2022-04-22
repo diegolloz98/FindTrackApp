0861b8c2fa863e22f8b95c9eac303c204433cfec
diegolloz98 <is714905@iteso.mx>
2022-04-22 03:18:52 -0500

    eft

--- lib/content/listaFav/item_public.dart
+++ lib/content/listaFav/item_public.dart
@@ -4,7 +4,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 
 class ItemPublic extends StatefulWidget {
   final Map<String, dynamic> publicFData;
@@ -19,33 +19,64 @@ class _ItemPublicState extends State<ItemPublic> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Image.network(
-                "${widget.publicFData["urlImagen"]}",
-                fit: BoxFit.cover,
+      child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                child: Stack(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 16 / 13,
+                      child: Image.network(
+                        "${widget.publicFData["urlImagen"]}",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      left: 10,
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                        size: 30,
+                      ),
+                    ),
+                    Positioned( 
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)
+                          ),
+                          primary: Colors.blue[300]
+                        ),  
+                      onPressed: () async{
+
+                        if (!await launch("${widget.publicFData["song_link"]}")) throw 'Could not launch song link';
+                      },
+                      child: ListTile(
+                        title: new Center(
+                          child: new Text(
+                            "${widget.publicFData["title"]}",
+                          )
+                        ),
+                        subtitle: new Center(
+                          child: new Text(
+                            "${widget.publicFData["artista"]}"
+                          ),
+                        ),
+                      )
+                    ),
+                    ),
+                    
+                  ],
+                ),
               ),
-            ),
-            ListTile(
-              title: Center(child: Text("${widget.publicFData["title"]}")),
-              subtitle: Center(child: Text("${widget.publicFData["artista"]}")),
-              trailing: Wrap(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.favorite, color: Colors.red),
-                    tooltip: "Likes: ${widget.publicFData["like"]}",
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
     );
   }
 }
@@ -76,3 +107,4 @@ showAlertDialog(BuildContext context) {
     },
   );
 }
+
